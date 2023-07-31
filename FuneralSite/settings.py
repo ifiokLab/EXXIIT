4a6b015f7a9394c29b87673ddccce27a4d6d2d51
ifiokLab <ifiokudoh77@gmail.com>
2023-08-01 00:42:14 +0100

    new

--- FuneralSite/settings.py
+++ FuneralSite/settings.py
@@ -31,7 +31,7 @@ DEBUG = os.environ.get('DEBUG', default=False)
 # SECURITY WARNING: don't run with debug turned on in production!
 #
 
-ALLOWED_HOSTS = ['foreverr.app']
+ALLOWED_HOSTS = ['foreverr.app','beta-foreverr-app.onrender.com']
 
 
 # Application definition
