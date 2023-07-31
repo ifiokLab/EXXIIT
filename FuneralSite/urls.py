2d39de396f95ef46c813fec1e0a75276172a9822
ifiokLab <ifiokudoh77@gmail.com>
2023-07-31 20:00:51 +0100

    new

--- FuneralSite/urls.py
+++ FuneralSite/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 from allauth.account.views import SignupView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls,),
@@ -27,4 +28,5 @@ urlpatterns = [
 
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
