9d0516e7beb783fb93beab5cc0e069a653b2c527
ifiokLab <ifiokudoh77@gmail.com>
2023-08-01 10:17:27 +0100

    new

--- FuneralSite/urls.py
+++ FuneralSite/urls.py
@@ -19,6 +19,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 from allauth.account.views import SignupView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls,),
@@ -26,7 +28,13 @@ urlpatterns = [
     path('',include('FuneralApp.urls')),
     path('accounts/', include('allauth.urls')),
 
-]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]#+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
 
 #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += staticfiles_urlpatterns()
